fd347c3c8b9ea7641ab5735da4e7615cd498df60
Shajitha04 <146456964+Shajitha04@users.noreply.github.com>
2025-04-19 13:05:31 +0530

    Create SkinAiApllication.java

---
+++ SkinAiApllication.java
@@ -0,0 +1,11 @@
+package com.skinai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinAiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SkinAiApplication.class, args);
+    }
+    }
